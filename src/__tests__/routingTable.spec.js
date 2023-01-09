81ea23813a9d8cb50a8f9dd9b7a94c0085f05c18
konrad.po <konrad.po@masterborn.com>
2023-01-09 09:04:54 +0100

    test: fix failing routing table test

--- src/__tests__/routingTable.spec.js
+++ src/__tests__/routingTable.spec.js
@@ -6,12 +6,12 @@ describe('Routing table', () => {
   const routingTableInstance = routingTable({'existing': 80});
 
   it('Should resolve existing mapping', () => {
-    const existingUrl = 'http://example.com/existing';
+    const existingUrl = '/existing';
     expect(routingTableInstance.resolvePort(existingUrl)).to.eql(80);
   });
 
   it('Should return undefined on non-existing mapping', () => {
-    const nonExistingUrl = 'http://example.com/non-existing';
+    const nonExistingUrl = '/non-existing';
     expect(routingTableInstance.resolvePort(nonExistingUrl)).to.be.undefined;
 
   });
